58088a6fa76b9872a326559a0a4cdc6851679f87
arleybri18 <arleybri18@gmail.com>
2019-08-14 23:49:34 +0000

    [Fixed] pep8 fix

--- models/__init__.py
+++ models/__init__.py
@@ -12,7 +12,7 @@ from os import getenv
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
-    storage =  DBStorage()
+    storage = DBStorage()
     storage.reload()
 else:
     from models.engine.file_storage import FileStorage
