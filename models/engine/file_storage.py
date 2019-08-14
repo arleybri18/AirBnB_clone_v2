c34a25d4391b0dc5ce12b2452f1ce95ef8356ec0
Madez <781@holbertonschool.com>
2019-08-14 00:36:39 +0000

    [Feature] Implement delete object in filestorage and return all object of a class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,12 +20,20 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
         """
-        return self.__objects
+
+        list_dict = {}
+        if cls is not None:
+            for key in self.__objects.keys():
+                if cls.__name__ in key.split(".")[0]:
+                    list_dict[key] = __objects[key]
+                    return list_dict
+        else:
+            return self.__objects
 
     def new(self, obj):
         """sets __object to given obj
@@ -55,3 +63,10 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        if obj is not None:
+            key_obj = "{}.{}".format(type(obj).__name__, obj.id)
+            if key_obj in self.__objects.keys():
+                del self.__objects[key_obj]
+                self.save()
