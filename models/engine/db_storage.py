8895758bf4ccdf4b508d5b6bb7515f3351492cae
arleybri18 <arleybri18@gmail.com>
2019-08-14 21:05:07 +0000

    [Feature] add db_storage.py

---
+++ models/engine/db_storage.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This is the db storage class for AirBnB"""
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from sqlalchemy import (create_engine)
+from slqalchemy import MetaData, Table
+
+
+class DBStorage:
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_HOST, HBNB_MYSQL_DB), pool_pre_ping=True)
+        if HBNB_ENV == 'test':
+            metadata = MetaData()
+            metadata.drop_all(engine)
