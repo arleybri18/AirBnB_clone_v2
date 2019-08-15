62471f9494edc0a10b0439723f29d6bab7bab547
Madez <781@holbertonschool.com>
2019-08-15 23:22:43 +0000

    [Fixed] - Create value create an update by default

--- models/base_model.py
+++ models/base_model.py
@@ -35,15 +35,14 @@ class BaseModel:
             created_at: creation date
             updated_at: updated date
         """
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = dt.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = dt.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = dt.now()
 
     def __str__(self):
         """returns a string
