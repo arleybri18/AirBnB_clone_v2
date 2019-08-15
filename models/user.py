f7736caeb9443e109852df3415cb0be41507429a
Madez <781@holbertonschool.com>
2019-08-15 02:45:31 +0000

    [Create] - class user and atribute

--- models/user.py
+++ models/user.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
 class User(BaseModel):
-    """This is the class for user
+    """Class state inherit from Base
     Attributes:
         email: email address
         password: password for you login
         first_name: first name
         last_name: last name
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
