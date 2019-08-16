3236cbd6df4f48a0ba4cd4e12b8621721bd10273
arleybri18 <arleybri18@gmail.com>
2019-08-16 15:48:45 +0000

    [fixed] pep8

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -13,20 +13,26 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from os import getenv
 
 HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
-HBNB_MYSQL_PWD =  getenv('HBNB_MYSQL_PWD')
+HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
 HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
 HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
 HBNB_ENV = getenv('HBNB_ENV')
+
 
 class DBStorage:
     __engine = None
     __session = None
 
     def __init__(self):
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_HOST, HBNB_MYSQL_DB), pool_pre_ping=True)
+        self.__engine = create_engine(
+            'mysql+mysqldb://{}:{}@{}/{}'.format(
+                HBNB_MYSQL_USER,
+                HBNB_MYSQL_PWD,
+                HBNB_MYSQL_HOST,
+                HBNB_MYSQL_DB),
+            pool_pre_ping=True)
         if HBNB_ENV == 'test':
             Base.metadata.drop_all(bind=self.__engine)
-
 
     def all(self, cls=None):
         """method must return a dictionary like file storage"""
@@ -44,24 +50,31 @@ class DBStorage:
                 for iter in query:
                     dict_query[key + iter.id] = iter
             return dict_query
-    
+
     def new(self, obj):
         """add the object to the current database session"""
         self.__session.add(obj)
-    
+
     def save(self):
         """commit all changes of the current database session"""
         self.__session.commit()
-    
+
     def delete(self, obj=None):
         """delete from the current database session obj if not None"""
         if obj:
-            delete_obj = session.query(type(obj)).filter(type(obj.id).like(obj.id))
+            delete_obj = session.query(
+                type(obj)).filter(
+                type(
+                    obj.id).like(
+                    obj.id))
             session.delete(delete_obj)
             self.save()
-    
+
     def reload(self):
         """create all tables in the database (feature of SQLAlchemy) and
         create the current database session"""
         Base.metadata.create_all(self.__engine)
-        self.__session = scoped_session(sessionmaker(bind=self.__engine, expire_on_commit=False))()
+        self.__session = scoped_session(
+            sessionmaker(
+                bind=self.__engine,
+                expire_on_commit=False))()
