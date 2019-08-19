27d8a8c1058fc2d6a0e073f7056151f4679258ab
arleybri18 <arleybri18@gmail.com>
2019-08-19 23:15:54 +0000

    [Feature] Script for deployment static content

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# This script configure my server for webstatic
+
+# create directories and file
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+#create file for test
+index_page="<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>"
+echo -e "$index_page" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+# create symbolic links
+symln=/data/web_static/current
+if [ -L $symln ]
+then
+	sudo unlink /data/web_static/current
+fi
+sudo ln -snf /data/web_static/releases/test/ /data/web_static/current
+# change user and group 
+sudo  chown -R ubuntu:ubuntu /data/
+# modify nginx configuration to server static content
+location="\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
+sudo sed -i "37i\ $location" /etc/nginx/sites-available/default
+sudo service nginx restart
