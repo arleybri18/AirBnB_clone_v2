9df9bbe747082fb1dc55f6f8d41e53c6a82baafa
arleybri18 <arleybri18@gmail.com>
2019-08-20 01:14:16 +0000

    [Feature] script that pak the web static content

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# this script generate .tgz from the contents of the web_static
+from fabric.api import local
+
+
+def do_pack():
+    """ Create a tar gz file
+
+    """
+    local("mkdir -p ./versions")
+    comp = local(
+        "tar -cvzf versions/web_static_$(date '+%Y%m%d%H%M%S').tgz web_static")
+
+    if (comp.succeeded):
+        return local("echo \"versions/web_static_$(date '+%Y%m%d%H%M%S').tgz\"")
+    else:
+        return None
