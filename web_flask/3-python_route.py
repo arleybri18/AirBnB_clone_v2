5ae25849ec97306d3cbbbf6a7d1b457782a7ae91
Yony Brinez <arleybri18@gmail.com>
2019-09-04 06:04:47 +0000

    Python routes defaults

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Import flask class """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """ Function to handle request """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello_hbnb():
+    """ Function to handle request to path /hbnb """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_route(text):
+    """ Function to handle request with a variable """
+    return 'C %s' % text.replace('_', ' ')
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python(text='is cool'):
+    """ Function to handle request with a variable and data default """
+    return 'Python %s' % text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
