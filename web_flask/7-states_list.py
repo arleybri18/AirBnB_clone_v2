32030701cbc2bcb2f2baa6a39a245b983e8b958e
Arley Brinez <arleybri18@gmail.com>
2019-09-05 03:31:11 +0000

    Fixed file

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """ Import flask class """
 from flask import Flask, render_template
-from models import storage
+from models import storage, State
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/states_list')
 def states_list():
     """ Function to handle request """
-    states = storage.all(State)    
-    return render_template('7-states_list.html', states = states) 
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def storage_close(exception=None)
+def storage_close(exception=None):
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
